a0f56aa1a21b191c919e2123d4b107ce128bf9e2
Utkarsh Singh <utkarshhh1511@gmail.com>
2026-08-16 22:12:29 +0530

    add bucket4j rate limiter

--- src/main/java/com/utkarsh/ratelimiterlab/controller/RateLimiterController.java
+++ src/main/java/com/utkarsh/ratelimiterlab/controller/RateLimiterController.java
@@ -1,6 +1,7 @@
 package com.utkarsh.ratelimiterlab.controller;
 
 
+import com.utkarsh.ratelimiterlab.limiter.bucket4j.Bucket4jLimiter;
 import com.utkarsh.ratelimiterlab.limiter.manual.TokenBucketLimiter;
 import com.utkarsh.ratelimiterlab.model.RateLimitResult;
 import lombok.Getter;
@@ -13,12 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/rate-limit")
 public class RateLimiterController {
     private final TokenBucketLimiter tokenBucketLimiter;
-    public RateLimiterController(TokenBucketLimiter tokenBucketLimiter) {
+    private final Bucket4jLimiter bucket4jLimiter;
+    public RateLimiterController(TokenBucketLimiter tokenBucketLimiter, Bucket4jLimiter bucket4jLimiter) {
         this.tokenBucketLimiter = tokenBucketLimiter;
+        this.bucket4jLimiter = bucket4jLimiter;
     }
 
     @GetMapping("/manual")
     public RateLimitResult test(@RequestParam String clientId){
         return tokenBucketLimiter.allow(clientId);
+    }
+
+    @GetMapping("/bucket4j")
+    public RateLimitResult testBucket4j(@RequestParam String clientId){
+        return bucket4jLimiter.allow(clientId);
     }
 }
